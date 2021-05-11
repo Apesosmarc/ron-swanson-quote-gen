25e7194c32f192ed19affe8119368b52228e3deb
Marc Apesos <apesos2013@gmail.com>
2021-05-11 12:07:45 -0400

    added media queries for small screen responsiveness.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const body = document.querySelector("body");
 const quoteSec = document.querySelector("#swansonQuote");
 const button = document.querySelector("button");
 const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
-const getQuote = async (url) => {
+const getQuote = async (quoteSec) => {
   const res = await axios.get(
     "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
   );
